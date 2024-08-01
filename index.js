06746ef4b5badcd9e33ae035e3dce8728d8b51cb
Ryukendo-Rahul <rahulmandal723202@gmail.com>
2024-08-02 01:11:06 +0530

    Added Features

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const profilePhotos = document.getElementsByClassName('profilephoto');
 const goHome = document.getElementsByClassName('homePage');
+const openReels = document.getElementsByClassName('openReel');
+const opentheSearch = document.getElementsByClassName('openSearch');
+
 
 // Iterate over the collection and add event listener to each element
 for (let i = 0; i < profilePhotos.length; i++) {
@@ -12,6 +15,32 @@ for (let i = 0; i < profilePhotos.length; i++) {
 function openProfilePage() {
   location.href = 'profile.html';
 }
+for (let i = 0; i < opentheSearch.length; i++) {
+  opentheSearch[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    openSearchPage();
+  });
+}
+
+function openSearchPage() {
+  location.href = 'search.html';
+}
+
+
+
+for (let i = 0; i < openReels.length; i++) {
+  openReels[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    openReelPage();
+  });
+}
+
+function openReelPage() {
+  location.href = 'reels.html';
+}
+
+
+
 
 for (let i = 0; i < goHome.length; i++) {
   goHome[i].addEventListener('click', function(event) {
