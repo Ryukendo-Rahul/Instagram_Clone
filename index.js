f0cb18b8ea3a07a684bf2d78458f856c208e788a
Ryukendo-Rahul <rahulmandal723202@gmail.com>
2024-08-01 19:50:58 +0530

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const profilePhotos = document.getElementsByClassName('profilephoto');
+const goHome = document.getElementsByClassName('homePage');
 
 // Iterate over the collection and add event listener to each element
 for (let i = 0; i < profilePhotos.length; i++) {
@@ -10,4 +11,15 @@ for (let i = 0; i < profilePhotos.length; i++) {
 
 function openProfilePage() {
   location.href = 'profile.html';
+}
+
+for (let i = 0; i < goHome.length; i++) {
+  goHome[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    openHomePage();
+  });
+}
+
+function openHomePage() {
+  location.href='index.html';
 }
